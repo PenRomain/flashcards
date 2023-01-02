6d7a55be5f71debb772139fdca9b0f2be49c1d34
PenRomain <romandreevich@icloud.com>
2023-01-02 22:01:15 +0300

    1/3 topics

--- View.js
+++ View.js
@@ -20,17 +20,19 @@ class View {
     const userAnswer = readLineSync
       .question(`\n${question[this.counter]}\n`)
       .toLowerCase();
-    if (userAnswer === question[this.counter + 1]) {
-      console.log('Right!');
-      this.counter += 3;
+    if (userAnswer === question[this.counter + 1].toLowerCase()) {
+      console.log(question[this.counter + 2]);
+      this.counter += 4;
+      if (question[this.counter + 1] === undefined) {
+        return true;
+      }
       this.showQuestion(question);
     }
-    return userAnswer;
   }
 
   showResult(question) {
     console.log(
-      `Вы ответили на ${this.counter / 3} из ${question.length / 3}!`
+      `Вы ответили на ${this.counter / 4} из ${question.length / 4}!`
     );
   }
 }
